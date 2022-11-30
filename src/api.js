543d2750e1a3e0d9fd612e7f55bd2a3848dd878b
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2022-11-29 21:27:44 -0500

    updated #1

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5ed72d08dfmshdb12f56df6c3b2cp1151f4jsn6eabcef0b54c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d4e868e02506ef400471a351fdf87bf6";
